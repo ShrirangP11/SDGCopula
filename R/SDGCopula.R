9926b77043164f0789c11abc2e01a46b137f379a
ShrirangP11 <160141361+ShrirangP11@users.noreply.github.com>
2024-11-02 18:58:10 +0530

    Update SDGCopula.R

--- R/SDGCopula.R
+++ R/SDGCopula.R
@@ -1,5 +1,5 @@
 # Prerequisite packages
-.packages = c('MASS', 'mvtnorm', 'copula', 'fitdistrplus', 'plotrix', 'psych', 'ggcorrplot', 'gridExtra', 'reshape2', 'ggplot2', 'lsa')
+.packages = c('mvtnorm', 'copula', 'fitdistrplus', 'plotrix', 'psych', 'ggcorrplot', 'gridExtra', 'reshape2', 'ggplot2', 'lsa', 'MASS')
 # Install CRAN packages (if not already installed)
 .inst <- .packages %in% installed.packages()
 if(length(.packages[!.inst]) > 0) install.packages(.packages[!.inst])
